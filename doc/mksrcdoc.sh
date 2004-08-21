29027724c87bfe368e15a2a8988f8627b28fb22d
mtr@daa9f17c-25e2-0310-a86f-979c7ba3dbcc <mtr@daa9f17c-25e2-0310-a86f-979c7ba3dbcc>
2004-08-21 08:23:37 +0000

    Small documentation fix.

--- doc/mksrcdoc.sh
+++ doc/mksrcdoc.sh
@@ -9,7 +9,7 @@ cat /dev/null > include_src.tex;
 
 for f in `cat lst_src.txt |grep -v "^#" |tr '\n' ' '`; do
     g=`echo $f |sed 's/^src\///g' -`; 
-    lgrind -i -c -lpy src/$f > doc_src/$g.tex;
+    lgrind -i -c -lpy src/$f > src_doc/$g.tex;
     h=`echo $g |sed 's/\_/\\\_/g' - `;
-    echo "\\section{$h}\\lgrindfile{doc_src/$g.tex}" >> include_src.tex;
+    echo "\\section{$h}\\lgrindfile{src_doc/$g.tex}" >> include_src.tex;
 done
