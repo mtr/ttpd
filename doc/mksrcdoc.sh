d03644c9f06e16b03e90d3135352ce287db54c0e
mtr@daa9f17c-25e2-0310-a86f-979c7ba3dbcc <mtr@daa9f17c-25e2-0310-a86f-979c7ba3dbcc>
2004-08-21 14:15:11 +0000

    Fixed API documentation.

--- doc/mksrcdoc.sh
+++ doc/mksrcdoc.sh
@@ -5,11 +5,20 @@
 # Copyright (C) 2002 by Martin Thorsen Ranang
 #
 
-cat /dev/null > include_src.tex; 
+docdir="src_doc"
+modules="../TTP/ SocketServer"
+includefile="include_src.tex"
 
-for f in `cat lst_src.txt |grep -v "^#" |tr '\n' ' '`; do
-    g=`echo $f |sed 's/^src\///g' -`; 
-    lgrind -i -c -lpy src/$f > src_doc/$g.tex;
-    h=`echo $g |sed 's/\_/\\\_/g' - `;
-    echo "\\section{$h}\\lgrindfile{src_doc/$g.tex}" >> include_src.tex;
-done
+epydoc --latex --output $docdir $modules
+
+cat $docdir/api.tex |grep '\include' - |sed "s/\\include{/\\input{$docdir\//g" - > $includefile
+
+
+#cat /dev/null > include_src.tex; 
+#
+#for f in `cat lst_src.txt |grep -v "^#" |tr '\n' ' '`; do
+#    g=`echo $f |sed 's/^src\///g' -`; 
+#    lgrind -i -c -lpy src/$f > src_doc/$g.tex;
+#    h=`echo $g |sed 's/\_/\\\_/g' - `;
+#    echo "\\section{$h}\\lgrindfile{src_doc/$g.tex}" >> include_src.tex;
+#done
