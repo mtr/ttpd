3105f33d5d51a13749afafb39207c033cff03843
mtr@daa9f17c-25e2-0310-a86f-979c7ba3dbcc <mtr@daa9f17c-25e2-0310-a86f-979c7ba3dbcc>
2004-08-31 14:41:27 +0000

    Improved analytical tool.

---
+++ statistics/last_52_weeks.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+#
+# $Id$
+#
+# Copyright (C) 2004 by Martin Thorsen Ranang
+#
+
+CONFPATH=$HOME/statistics
+
+. $CONFPATH/general_config.sh
+
+RESOLUTION=weeks
+year=`date +%Y`
+week=$((`date +%V` + 1))
+
+if [ $week -gt 53 ]; then
+    week=1;
+    year=$(( $year + 1 ));
+fi
+
+WEEK=$year,$week
+FNAME=last_52_weeks_$RESOLUTION
+
+ttpd_analyze $LOGS \
+    --restrict-to=interface=$INTERFACE,host=$HOST,trans_type=$TRANS_TYPE \
+    --resolution=$RESOLUTION \
+    --week=$WEEK \
+    --chart=$EXPORT/graphics/$FNAME.png \
+    > $EXPORT/$FNAME.txt
+
+#RESOLUTION=days
+#FNAME=last_52_weeks_$RESOLUTION
+
+#ttpd_analyze $LOGS \
+#    --restrict-to=interface=$INTERFACE,host=$HOST,trans_type=$TRANS_TYPE \
+#    --resolution=$RESOLUTION \
+#    --week=$WEEK \
+#    --chart=$EXPORT/graphics/$FNAME.png \
+#    > $EXPORT/$FNAME.txt
