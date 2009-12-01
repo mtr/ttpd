6b8557b3e431d9347e702dca8d97d61b1cda72e9
mtr@daa9f17c-25e2-0310-a86f-979c7ba3dbcc <mtr@daa9f17c-25e2-0310-a86f-979c7ba3dbcc>
2009-12-01 10:21:07 +0000

    Cleaned up docstring formatting.

--- TTP/LogHandler.py
+++ TTP/LogHandler.py
@@ -27,39 +27,33 @@ log_levels = dict([(logging.getLevelName(l).lower(), l)
                    for l in logging._levelNames if type(l) == types.IntType])
 
 def get_log_levels():
-
-    """ Return a sorted list of log levels. """
-
+    """Return a sorted list of log levels.
+    """
     items = [(v, k) for k, v in log_levels.items()]
     items.sort()
     return [k for v, k in items]
     
 class DaemonFileHandler(logging.FileHandler):
-
-    """ A log file handler implementation well suited for daemon
-    usage.  It's considered nice behavior of a daemon to reopen its
-    log files when it recieves a certain signal (usually SIGHUP)."""
-    
-    def reopen(self, mode = 'a'):
-        
-        """ Reopen the log file.  This is necessary for rotating logs
-        without restarting the whole daemon. """
-        
+    """A log file handler implementation well suited for daemon usage.
+    It's considered nice behavior for a daemon to reopen its log files
+    when it recieves a certain signal (usually SIGHUP).
+    """
+    def reopen(self, mode='a'):
+        """Reopen the log file.  This is necessary for rotating logs
+        without restarting the whole daemon.
+        """
         new_stream = open(self.baseFilename, self.mode)
         self.stream.close()
         self.stream = new_stream
-        
 
 class LogHandler(DaemonFileHandler):
-    
-    """ A handler class which writes formatted logging records to disk
-    files. """
-    
-    def __init__(self, filename, mode = 'a'):
-        
-        """ Open the specified file and use it as the stream for
-        logging.  """
-        
+    """A handler class which writes formatted logging records to disk
+    files.
+    """
+    def __init__(self, filename, mode='a'):
+        """Open the specified file and use it as the stream for
+        logging.
+        """
         DaemonFileHandler.__init__(self, filename, mode)
         
         self.setFormatter(logging.Formatter('%(asctime)s %(name)s ' \
@@ -68,9 +62,8 @@ class LogHandler(DaemonFileHandler):
                                             '%Y-%m-%d %H:%M:%S'))
         
 def main ():
-
-    """ Module mainline (for standalone execution). """
-    
+    """Module mainline (for standalone execution).
+    """
     return
 
 if __name__ == "__main__":
