de0c3602361db08ce2d1df040731c01a8d5d7d62
mtr@daa9f17c-25e2-0310-a86f-979c7ba3dbcc <mtr@daa9f17c-25e2-0310-a86f-979c7ba3dbcc>
2009-12-04 11:37:10 +0000

    Updated the release procedure.

--- release.sh
+++ release.sh
@@ -3,8 +3,13 @@
 
 #export COMMIT_BEFORE_RELEASE=0
 
+svn update
+
 # Update ChangeLog
 svn2cl --break-before-msg --group-by-day --authors=AUTHORS
+
+automake
+autoconf
 
 ./configure --prefix=/usr && \
 make deb_sign && \
