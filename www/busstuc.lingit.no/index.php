c9833bb847f5308e84248ec23fcc6df4ac05762e
ubuntu@ip-10-227-135-34.eu-west-1.compute.internal <ubuntu@ip-10-227-135-34.eu-west-1.compute.internal>
2010-02-22 21:09:00 +0100

    Cleaned up Web pages.

--- www/busstuc.lingit.no/index.php
+++ www/busstuc.lingit.no/index.php
@@ -1,26 +1,29 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
 
-<html> <head>
-<title>SMSTUC</title>
-<META  name="description" 
-content="A natural language system capable of answering questions about bus
-departures in Trondheim, Norway. Pose you questions here as complete
-english sentences.">
+<html> 
+  <head>
+    <title>SMSTUC</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <meta name="description" 
+	  content="A natural language system capable of answering
+		   questions about bus departures in Trondheim,
+		   Norway. Pose your questions here as complete
+		   English sentences.">
 
-<META  name="keywords" content="natural language processing, NLP, Bustuc,
-Tuc, The Understanding Computer, Sicstus prolog, artificial
-intelligence, parsing, automated reasoning, knowledge based systems,
-expert systems">
+    <meta name="keywords" content="natural language processing, NLP, Bustuc,
+				   TUC, The Understanding Computer,
+				   SICStus Prolog, artificial
+				   intelligence, parsing, automated
+				   reasoning, knowledge based systems,
+				   expert systems">
 
 </head>
 
-<FRAMESET rows="320,*">
+<frameset rows="320,*">
   <frame src="bust.php" name="smstuc" 
    frameborder="0" marginwidth="10" marginheight="10">
   <frame src="instruksjoner.php" name="ans"
    frameborder="0" marginwidth="10" marginheight="30">
-</FRAMESET> 
-
 <noframes>
 <body <?PHP include('body_style.inc'); ?> >
 
@@ -30,7 +33,7 @@ expert systems">
 spørsmål</a><p>
 <a href="ombustuc.php" target="ans">Om Busstuc</a><p>
 <a href="egenskaper.php" target="ans">Egenskaper ved Busstuc</a>
-<img hspace=10 src="../logo/ntnu-logo1.gif">
+<img hspace=10 src="../logo/ntnu-logo1.gif" alt="NTNU-logo">
 <a href="../bustuc" target=_top>[English version]</a><p>
 <i>Versjon av 26.2.02 </i>
 
@@ -38,7 +41,7 @@ spørsmål</a><p>
 
 <hr>
 <address>
-<A Href="mailto:tagore@idi.ntnu.no"> tagore@idi.ntnu.no</A>
+<a href="mailto:tagore@idi.ntnu.no"> tagore@idi.ntnu.no</a>
 </address>
 <!-- hhmts start -->
 Last modified: Mon Aug 23 20:11:52 CEST 2004
@@ -46,4 +49,6 @@ Last modified: Mon Aug 23 20:11:52 CEST 2004
 </body>
 
 </noframes>
+</frameset> 
+
 </html>
