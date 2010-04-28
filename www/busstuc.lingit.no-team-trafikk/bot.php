1eac39b5eaf74c898bc0994d8c7dc1d6c95b7a16
ubuntu@ip-10-227-58-31.eu-west-1.compute.internal <ubuntu@ip-10-227-58-31.eu-west-1.compute.internal>
2010-04-28 14:43:42 +0200

    Continued with the Team Trafikk-tailored Web page versions.

--- www/busstuc.lingit.no-team-trafikk/bot.php
+++ www/busstuc.lingit.no-team-trafikk/bot.php
@@ -1,16 +1,21 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
   <head>
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <meta http-equiv="Content-Type" content="text/html;charset=iso-8859-1">
     <title></title>
   </head>
 
-<body <?PHP include('body_style.inc'); ?> >
+  <body
+text=#000000
+link =#600060
+vlink=000080
+alink=paleturquoise
+bgcolor=#c0c0c0
+>
+    <h1>Svar på spørsmålet:</h1>
 
-<h1>Svar pÃ¥ spÃ¸rsmÃ¥let:</h1>
+    <?PHP include("tuc.inc"); ?>
 
-<?PHP include("tuc.inc"); ?>
-
-<hr>
-</body>
+    <hr>
+  </body>
 </html>
