51f7b90932f68e1bc99aa47f388f657a779f3034
Martin Thorsen Ranang <mtr@ranang.org>
2010-08-06 15:21:57 +0200

    Added a JSON Web interface.

--- www/busstuc.lingit.no/json.php
+++ www/busstuc.lingit.no/json.php
@@ -5,7 +5,6 @@ $quest = $_POST["question"];
 if ($quest)
   {
     # Tegn som skal fjærnes fra input.
-    
     $removes = array("'", '"', '\\', '`', '´', '[', ']');
 
     # "Snillifiserer" input:
@@ -14,47 +13,33 @@ if ($quest)
     # - strip_tags fjerner eventuelle HTML (eller XML)-tags.
     # - str_replace fjerner tegnene som ramses opp ovenfor.
     # - trim fjerner eventuelle white-space i starten og slutten av strengen.
-    
     $e = trim(str_replace($removes, "", strip_tags(substr($quest, 0, 1024))));
 
     # Dersom strengen nå inneholder "ekle" tegn, vil vi ikke behandle den.
-    
     if (! ereg("^[*a-zA-ZæøåÆØÅéÉöÖäÄ0-9,. ?!@:+-/]*$", $e))
       {
 	unset($e);
       }
   }
 
+header('Cache-Control: no-cache, must-revalidate');
+header('Content-type: text/x-json;charset=utf-8');
+
 if ($e)
   {
     # Dersom det mangler et '.' i slutten av setningen, legg det til.
-    
     if ($e && (! ereg("[.?!]$", $e)))
       {
 	$e = $e.".";
       }
     
-    printf("<ul><li>%s</li>", $e);
-    
     setlocale(LC_CTYPE, "UTF8", "nb_NO.UTF-8");
     $e = escapeshellarg($e);
     
-    print "<li>";
-    if ($_POST["tekniskinfo"])
-      {
-	printf("<pre>");
-	system("$tech_prog $e 2>&1");
-	printf("</pre>");
-      }
-    else
-      {
-	system("$prog $e");
-      }
-    
-    print "</li></ul>";
+    system("$prog $e");
   }
  else
    {
-     print "Du må taste inn et spørsmål før du trykker på \"Send spørsmål\".";
+     print '{"error": "No question supplied."}';
    }
 ?>
---
+++ www/busstuc.lingit.no/json_form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
+
+<html> 
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <title>SMSTUC JSON Form</title>
+  </head>
+  <body>
+    <form method="post" action="./json.php">
+      <input type=text name="question" value="" size="79"> 
+      <input type=submit value="Submit Question">
+    </form>
+  </body>
+</html>
